10721bd13704a013e0fca8791b2fb319bc03d770
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2020-03-08 20:38:08 -0300

    File generator

---
+++ files/generateFiles.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def generate_static(n, l, name, r=0.25):
+    f = open(name, 'w')
+    one_value = '{}\n'
+    f.write(one_value.format(n))
+    f.write(one_value.format(l))
+    prop = 1
+    for x in range(0,n):
+        f.write('{} {}\n'.format(r, prop))
+
+def generate_dynamic(n, l, name):
+    f = open(name, 'w')
+    f.write('0\n')
+    for x in range(0, n):
+        f.write('{} {}\n'.format(np.random.uniform(0, l), np.random.uniform(0,l)))
+
+def generate_files(index, n, l):
+    generate_static(n, l,'static-' + str(index) + '.txt')
+    generate_dynamic(n, l,'dynamic-' + str(index) + '.txt')
+
+n = [10,20,50,100,200,300,500,1000]
+
+i = 0
+for x in n:
+    i += 1
+    generate_files(i, n[i-1], 20)
